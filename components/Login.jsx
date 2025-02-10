09d8593427354cbb12b3f44fc2586311fdb80a7a
aswinr-rr <aswinforpc12@gmail.com>
2025-02-10 10:06:53 +0530

    solveed

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,10 +1,63 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View } from "react-native";
+import { Colors } from "@/constants/Colors";
 
 export default function Login() {
   return (
     <View>
-      <Text>Login</Text>
+      <Image 
+        source={require('./../assets/images/landing.jpg')}
+        style={{
+          width: '100%',
+          height: 400,
+        }} 
+      />
+      <View style={styles.container}>
+        <Text style={{
+          fontSize: 25,
+          fontFamily: 'outfit-bold',
+          textAlign: "center",
+          color: Colors.BLUE1, 
+        }}>
+          AI Travel Planner
+        </Text>
+        <Text style={{
+          fontFamily: 'outfit',
+          fontSize: 18,
+          textAlign: "center",
+          color: Colors.BLUE2,
+        }}>
+          Plan your perfect trip effortlessly with AI-powered itineraries tailored to your preferences, budget, and real-time updates!
+        </Text>
+        <View style={styles.button}>
+          <Text style={{
+            color: Colors.WHITE,
+            textAlign: "center",
+            fontFamily: 'outfit',
+            fontSize: 16,
+          }}>
+            Sign In With Google
+          </Text>
+        </View>
+      </View>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.WHITE,
+    marginTop: -10,
+    height: '100%',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    padding: 25,
+  },
+  button: {
+    padding: 17,
+    width: 230,
+    marginLeft: 38,
+    backgroundColor: Colors.BLUE1,
+    borderRadius: 99,
+    marginTop: '17%',
+  },
+});
